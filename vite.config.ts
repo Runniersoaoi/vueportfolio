63f170c5c85c06a715433508f44bb61d54aed0f9
Runniersoaoi <matiasaquinozc@gmail.com>
2024-12-11 21:53:22 -0500

    feat: vite configuration change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Runniersoaoi.github.io/vueportfolio/',
+  base: 'Runniersoaoi/vueportfolio/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
